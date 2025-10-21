2f48b51d8b733556655c3987c8d3e6870654b32d
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-10-21 14:26:48 +0530

    add custom logo support to theme
    Implemented wearl_technologies_custom_theme_logo_setup() function
    
    Added default logo dimensions (height: 50, width: 200)
    
    Hooked setup to 'after_setup_theme' action
    
    Enabled WordPress custom logo feature for theme branding

--- Lec-2/SecondApp/wp-content/themes/Wearl Technologies Custom theme/functions.php
+++ Lec-2/SecondApp/wp-content/themes/Wearl Technologies Custom theme/functions.php
@@ -25,4 +25,19 @@ function register_wearl_technologies_theme(){
     add_action("init", "register_wearl_technologies_theme");
 
 
+// logo default setup
+
+function wearl_technologies_custom_theme_logo_setup(){
+    $defaults = array(
+        'height'      => 50,
+        'width'       => 200,
+        // 'flex-height' => true,
+        // 'flex-width'  => true,
+        // 'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+
+add_action( 'after_setup_theme', 'wearl_technologies_custom_theme_logo_setup' );
+
 ?>
