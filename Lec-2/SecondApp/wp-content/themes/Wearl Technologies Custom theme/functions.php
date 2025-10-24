b247f2a53571c3ea96e63a2bdd39589eaff0897d
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-10-24 23:43:30 +0530

    add custom sidebar widget area for Wearl Technologies theme
    Created 'Widget Area' registered under ID 'custom-sidebar'
    
    Added HTML structure for widgets and titles
    
    Included description and localization support
    
    Hooked sidebar registration to 'widgets_init' action

--- Lec-2/SecondApp/wp-content/themes/Wearl Technologies Custom theme/functions.php
+++ Lec-2/SecondApp/wp-content/themes/Wearl Technologies Custom theme/functions.php
@@ -58,4 +58,22 @@ function register_custom_post_type(){
 }
  add_action('init', 'register_custom_post_type');
 
+
+ //create custom sidebar
+    function wearl_technologies_custom_theme_sidebar(){
+        register_sidebar(
+            array(
+                'name' => __('Widget Area', 'wearl-technologies-custom-theme'),
+                'id' => 'custom-sidebar',
+                'description' => __('A custom sidebar for Wearl Technologies Custom Theme', 'wearl-technologies-custom-theme'),
+                'before_widget' => '<div class="widget %2$s" id="%1$s">',
+                'after_widget' => '</div>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title' => '</h3>',
+            )
+        );
+    }
+
+add_action('widgets_init', 'wearl_technologies_custom_theme_sidebar');
+
 ?>
